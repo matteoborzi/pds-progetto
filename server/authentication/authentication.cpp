8d0d3a639a840b25f697d88e60311368493dadde
Roberto Bressani <roberto.bressani97@gmail.com>
2020-07-24 18:20:16 +0200

    src: added authenticate implementation

--- server/authentication/authentication.cpp
+++ server/authentication/authentication.cpp
@@ -1,10 +1,43 @@
-//
-// Created by rober on 24/07/2020.
-//
-
+#include <fstream>
+#include <shared_mutex>
 #include "authentication.h"
 
-bool authenticate(std::string username, std::string password){
+std::pair<std::string, std::string> splitLine(std::string s);
+void addUser(std::fstream f, std::string user, std::string pw);
 
-    return false;
+static std::shared_mutex _authentication;
+
+bool authenticate(std::string username, std::string password){
+   std::shared_lock readL{_authentication};
+
+    std::fstream file{"authentication.txt", std::ios::in | std::ios::out | std::ios::app};
+    std::string line;
+
+
+    while(std::getline(file,line)) {
+        auto infos = splitLine(line);
+        if (infos.first == username) {
+            file.close();
+            if (infos.second == password)
+                return true;
+            return false;
+        }
+    }
+
+    readL.unlock();
+    //getting here when user is not found, adding and authenticating him
+    file << username <<  "\t" << password  << std::endl;
+    file.close();
+    return true;
+}
+
+std::pair<std::string, std::string> splitLine(std::string s){
+    return std::pair{"pippo", "password"};
+}
+
+void addUser(std::fstream f, std::string user, std::string pw){
+    std::unique_lock l{_authentication};
+
+    
+
 }
