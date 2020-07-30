0f23b66db56433049c3aaf47ead2a2a313b4ecdf
Angelica Urbanelli <s271114@studenti.polito.it>
2020-07-30 16:22:58 +0200

    src: added configuration files

---
+++ client/Configuration/Configuration.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Angelica on 30/07/2020.
+//
+
+#include "Configuration.h"
---
+++ client/Configuration/Configuration.h
@@ -0,0 +1,14 @@
+//
+// Created by Angelica on 30/07/2020.
+//
+
+#ifndef CLIENT_CONFIGURATION_H
+#define CLIENT_CONFIGURATION_H
+
+
+class Configuration {
+
+};
+
+
+#endif //CLIENT_CONFIGURATION_H
