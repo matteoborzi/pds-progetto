f838d5665ec5ca828e93a3ef460d0860bf8337fb
Angelica Urbanelli <s271114@studenti.polito.it>
2020-08-07 19:56:44 +0200

    test: added unit tests for Job class

---
+++ test/job.cpp
@@ -0,0 +1,41 @@
+#define BOOST_TEST_MODULE My Test
+
+#include <boost/test/included/unit_test.hpp>
+#include "../client/job/Job.h"
+#include "../client/job/JobQueue.h"
+//#define BOOST_TEST_DYN_LINK
+//#define BOOST_TEST_MAIN
+
+BOOST_AUTO_TEST_SUITE( test_suite_job )
+
+BOOST_AUTO_TEST_CASE( test_job_1 ){
+    Job job{"/root/folder1", ADD_DIRECTORY, true};
+
+    /* TODO
+     * aggiungere un controllo nel costruttore
+     * per vedere se Action e isFile sono coerenti
+     * (es. in questo caso NON sono coerenti)
+     */
+
+    BOOST_CHECK_EQUAL(job.getAct(), ADD_DIRECTORY);
+    BOOST_CHECK_EQUAL(job.getPath(), "/root/folder1");
+    BOOST_CHECK_EQUAL(job.getIsFile(), true);
+}
+
+BOOST_AUTO_TEST_CASE( test_job_2 ){
+    Job job{"/root/folder1", ADD_DIRECTORY, true};
+    BOOST_CHECK_EQUAL(job.getAct(), ADD_DIRECTORY);
+    job.setAct(ADD_FILE);
+    BOOST_CHECK_EQUAL(job.getAct(), ADD_FILE);
+}
+
+BOOST_AUTO_TEST_CASE(test_job_3){
+        Job job{"/root/folder1", ADD_DIRECTORY, true};
+        Job job2{"/root/folder1", ADD_DIRECTORY, false};
+        BOOST_CHECK(job != job2);
+
+        Job job3{"/root/folder1", ADD_DIRECTORY, true};
+        BOOST_CHECK(job == job3);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
