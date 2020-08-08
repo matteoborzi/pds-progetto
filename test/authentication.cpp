041b8ce71a5d81918964a34789cf92395f0b64b7
Angelica Urbanelli <s271114@studenti.polito.it>
2020-08-08 19:37:00 +0200

    test: added file to test authentication functionality

---
+++ test/authentication.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Angelica on 08/08/2020.
+//
+
+
+/* TODO
+ * in createUserFolder
+ * se non si riesce a creare una cartella
+ * forse sarebbe il caso di eliminare l'utente
+ * dal file DB
+ */
+
+/* TODO
+ * forse è opportuno controllare il nome utente
+ * al primo accesso
+ * ad esempio controllando che non ci siano / o altri caratteri
+ */
