18d90a199f5778998ce419a587b49f31ebf98b24
Angelica Urbanelli <s271114@studenti.polito.it>
2020-08-10 15:54:49 +0200

    test: added test suite authentication

--- test/authentication.cpp
+++ test/authentication.cpp
@@ -1,7 +1,12 @@
 //
 // Created by Angelica on 08/08/2020.
 //
-
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+#include <boost/test/unit_test.hpp>
+#include "../server/authentication/authentication.h"
+#include <thread>
+#include <iostream>
 
 /* TODO
  * in createUserFolder
@@ -14,4 +19,15 @@
  * forse è opportuno controllare il nome utente
  * al primo accesso
  * ad esempio controllando che non ci siano / o altri caratteri
+ * (anche se il controllo è già fatto lato client)
  */
+
+BOOST_AUTO_TEST_SUITE( test_suite_authentication);
+
+/*
+BOOST_AUTO_TEST_CASE(test_authentication_1){
+    bool res = authenticate("pippo", "password");
+    BOOST_CHECK(res == true);
+}
+*/
+BOOST_AUTO_TEST_SUITE_END();
