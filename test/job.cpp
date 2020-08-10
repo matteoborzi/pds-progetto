a49b53b7e284af644c60c55d7d5538fa64e918ff
Roberto Bressani <roberto.bressani97@gmail.com>
2020-08-10 11:33:56 +0200

    test: adapted to new check on  fileType and action

--- test/job.cpp
+++ test/job.cpp
@@ -9,15 +9,10 @@
 BOOST_AUTO_TEST_SUITE( test_suite_job )
 
 BOOST_AUTO_TEST_CASE( test_job_1 ){
-    Job job{"/root/folder1", ADD_DIRECTORY, true};
+    Job job{"/root/folder1", ADD_FILE, true};
 
-    /* TODO
-     * aggiungere un controllo nel costruttore e in setAct
-     * per vedere se Action e isFile sono coerenti
-     * (es. in questo caso NON sono coerenti)
-     */
 
-    BOOST_CHECK_EQUAL(job.getAct(), ADD_DIRECTORY);
+    BOOST_CHECK_EQUAL(job.getAct(), ADD_FILE);
     BOOST_CHECK_EQUAL(job.getPath(), "/root/folder1");
     BOOST_CHECK_EQUAL(job.getIsFile(), true);
 }
@@ -25,17 +20,33 @@ BOOST_AUTO_TEST_CASE( test_job_1 ){
 BOOST_AUTO_TEST_CASE( test_job_2 ){
     Job job{"/root/folder1", ADD_DIRECTORY, false};
     BOOST_CHECK_EQUAL(job.getAct(), ADD_DIRECTORY);
-    job.setAct(ADD_FILE);
-    BOOST_CHECK_EQUAL(job.getAct(), ADD_FILE);
+    job.setAct(UPDATE);
+    BOOST_CHECK_EQUAL(job.getAct(), UPDATE);
 }
 
 BOOST_AUTO_TEST_CASE(test_job_3){
-        Job job{"/root/folder1", ADD_DIRECTORY, true};
+        Job job{"/root/folder1", ADD_FILE, true};
         Job job2{"/root/folder1", ADD_DIRECTORY, false};
         BOOST_CHECK(job != job2);
 
-        Job job3{"/root/folder1", ADD_DIRECTORY, true};
+        Job job3{"/root/folder1", ADD_FILE, true};
         BOOST_CHECK(job == job3);
+}
+
+BOOST_AUTO_TEST_CASE(test_job_4){
+    BOOST_CHECK_THROW(
+        Job job("/root/folder1", ADD_FILE, false), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_job_5){
+        BOOST_CHECK_THROW(
+                Job job("/root/folder1", ADD_DIRECTORY, true), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_job_6){
+        Job job("/root/folder1", ADD_DIRECTORY, false);
+        BOOST_CHECK_THROW(
+                job.setAct(ADD_FILE), std::logic_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
