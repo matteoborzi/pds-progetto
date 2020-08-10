94f744b48cda7210122c82109b17dd2f4637b144
matteoborzi <s280104@studenti.polito.it>
2020-08-10 15:55:38 +0200

    src: add Checksum.h and Checksum.cpp stub

---
+++ common/Checksum.cpp
@@ -0,0 +1,10 @@
+#include <openssl/md5.h>
+#include "Checksum.h"
+
+std::string computeChecksum(const std::string &path){
+	std::string hash;
+
+		//TODO: compute the hash for each chunk
+
+	return hash;
+}
---
+++ common/Checksum.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_CHECKSUM_H
+#define COMMON_CHECKSUM_H
+
+#include<string>
+#include<fstream>
+
+std::string computeChecksum(const std::string &path);
+
+#endif // COMMON_CHECKSUM_H
