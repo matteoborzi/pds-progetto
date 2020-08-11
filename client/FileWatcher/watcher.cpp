4e62e1dba8ab70ce00e9b019c24f27d813881df9
Roberto Bressani <roberto.bressani97@gmail.com>
2020-08-11 19:20:28 +0200

    fix: added empty files

---
+++ client/FileWatcher/watcher.cpp
@@ -0,0 +1,9 @@
+//
+// Created by rober on 11/08/2020.
+//
+
+#include "watcher.h"
+
+void watch(JobQueue& queue){
+
+}
---
+++ client/FileWatcher/watcher.h
@@ -0,0 +1,12 @@
+//
+// Created by rober on 11/08/2020.
+//
+
+#ifndef CLIENT_WATCHER_H
+#define CLIENT_WATCHER_H
+
+#include "../job/JobQueue.h"
+
+void watch(JobQueue& queue);
+
+#endif //CLIENT_WATCHER_H
