3892530502a83bb9bdd7c5cff6c488e89c15f1a3
Roberto Bressani <roberto.bressani97@gmail.com>
2020-08-13 10:00:28 +0200

    src: added scheleton for watch function

--- client/FileWatcher/watcher.cpp
+++ client/FileWatcher/watcher.cpp
@@ -2,8 +2,22 @@
 // Created by rober on 11/08/2020.
 //
 
+#include <optional>
+#include <thread>
+#include <filesystem>
 #include "watcher.h"
+#include "../Configuration/Configuration.h"
 
 void watch(JobQueue& queue){
+    std::optional<Configuration> conf=Configuration::getConfiguration();
+    if(!conf.has_value())
+        throw std::runtime_error("Impossible to get configuration");
+    std::string abs_path = conf.value().getPath();
+    while(true){
+        for(auto element : std::filesystem::recursive_directory_iterator{abs_path}){
+            std::cout<<element<<std::endl;
+        }
 
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 }
