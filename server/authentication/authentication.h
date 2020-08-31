c60ab20cd47adceeb31975689d3d589987560f2f
matteoborzi <s280104@studenti.polito.it>
2020-08-31 17:33:24 +0200

    src: changed returned type of splitLine

--- server/authentication/authentication.h
+++ server/authentication/authentication.h
@@ -4,6 +4,6 @@
 #include <string>
 
 bool authenticate(std::string username, std::string password);
-std::pair<std::string, std::string> splitLine(std::string& s);
+std::pair<std::string, std::pair<std::string, std::string>> splitLine(std::string& s);
 
 #endif //SERVER_AUTHENTICATION_H
