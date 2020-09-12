77837e437eefe43368d9c22249e90d7c1b3654a1
mac <s233820@studenti.polito.it>
2020-09-12 18:22:37 +0200

    src: implement computeChecksum function

--- common/Checksum.cpp
+++ common/Checksum.cpp
@@ -1,10 +1,19 @@
-#include <openssl/md5.h>
+#include <crypto++/cryptlib.h>
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include <crypto++/md5.h>
+#include <crypto++/files.h>
+#include <crypto++/hex.h>
+#include <fstream>
 #include "Checksum.h"
 
 std::string computeChecksum(const std::string &path){
-	std::string hash;
-
-		//TODO: compute the hash for each chunk
-
-	return hash;
+	CryptoPP::Weak1::MD5 hash;
+	  const size_t size = CryptoPP::Weak1::MD5::DIGESTSIZE * 2;
+	  byte buf[size] = {0};
+	  CryptoPP::FileSource(
+		  path.c_str(), true,
+		  new CryptoPP::HashFilter(
+		      hash, new CryptoPP::HexEncoder(new CryptoPP::ArraySink(buf, size))));
+	  std::string checksum = std::string(reinterpret_cast<const char*>(buf), size);
+	return checksum;
 }
