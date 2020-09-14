c8f1a2cf223ef40e143bd2b0ff4c82fa29b285e6
Roberto Bressani <roberto.bressani97@gmail.com>
2020-09-14 19:51:38 +0200

    src: implemented first socket connection

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <boost/asio/ip/tcp.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    boost::asio::io_service io_service;
+//socket creation
+    boost::asio::ip::tcp::socket socket(io_service);
+//connection
+    socket.connect( boost::asio::ip::tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 8080 ));
+    return 2;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,27 @@
+#include <boost/asio/ip/tcp.hpp>
 #include <iostream>
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    boost::asio::io_service my_context;
+
+    boost::asio::ip::tcp::acceptor acceptor(my_context);
+    //TODO porta da linea di comando
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), 8080);
+    acceptor.open(endpoint.protocol());
+    acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+    acceptor.bind(endpoint);
+    acceptor.listen();
+    std::cout<<"I'm listening"<<std::endl;
+
+    while(true) {
+        boost::asio::ip::tcp::socket socket(my_context);
+
+        acceptor.accept(socket);
+        std::cout << "Get a connection" << std::endl;
+    }
+
+
+    return 1;
 }
