5070ab7113b41fc174ca3a3fe4b53ed2378a6519
Roberto Bressani <roberto.bressani97@gmail.com>
2020-09-20 12:36:11 +0200

    src: changed multiple inclusion header

--- common/messages/file_utils.h
+++ common/messages/file_utils.h
@@ -1,9 +1,9 @@
-#ifndef FILE_UTILS_H
-#define FILE_UTILS_H
+#ifndef FILE_UTILS_COMMON_H
+#define FILE_UTILS_COMMON_H
 #include <boost/asio/ip/tcp.hpp>
 #include <string>
 
 void sendFile(boost::asio::ip::tcp::socket &,const std::string&);
 void receiveFile(boost::asio::ip::tcp::socket &,const std::string&);
 
-#endif //SERVER_JOB_UTILS_H
+#endif //FILE_UTILS_COMMON_H
