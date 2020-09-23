5b0d8f63ac37737ea5eee4253b329ce37b65af7e
MacSpowman <matteo.borzi@gmail.com>
2020-09-23 19:52:57 +0200

    src: add check on file existence in Checksum

--- common/Checksum.cpp
+++ common/Checksum.cpp
@@ -4,17 +4,25 @@
 #include <crypto++/files.h>
 #include <crypto++/hex.h>
 #include <fstream>
+#include <filesystem>
 #include "Checksum.h"
 
 std::string computeChecksum(const std::string &path){
 	CryptoPP::Weak1::MD5 hash;
-	//TODO add checks if file exists
-	  const size_t size = CryptoPP::Weak1::MD5::DIGESTSIZE * 2;
-	  byte buf[size] = {0};
-	  CryptoPP::FileSource(
-		  path.c_str(), true,
-		  new CryptoPP::HashFilter(
-		      hash, new CryptoPP::HexEncoder(new CryptoPP::ArraySink(buf, size))));
-	  std::string checksum = std::string(reinterpret_cast<const char*>(buf), size);
+
+	std::filesystem::directory_entry file{path};
+
+	if(!file.exists()){
+	    throw std::runtime_error("Checksum computation: file " + path + " does not exist");
+	}
+
+	const size_t size = CryptoPP::Weak1::MD5::DIGESTSIZE * 2;
+	byte buf[size] = {0};
+	CryptoPP::FileSource(
+	        path.c_str(), true,
+	        new CryptoPP::HashFilter(
+	                hash, new CryptoPP::HexEncoder(new CryptoPP::ArraySink(buf, size))));
+    std::string checksum = std::string(reinterpret_cast<const char*>(buf), size);
+
 	return checksum;
 }
